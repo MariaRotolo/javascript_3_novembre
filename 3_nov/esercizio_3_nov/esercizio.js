8a3bd56b6f900a403159ff8397f6bf35edc5185b
MariaRotolo <92733014+MariaRotolo@users.noreply.github.com>
2021-11-03 18:33:42 +0100

    Update esercizio.js

--- 3_nov/esercizio_3_nov/esercizio.js
+++ 3_nov/esercizio_3_nov/esercizio.js
@@ -1,5 +1,3 @@
-
-
 document.addEventListener("DOMContentLoaded", () => {  
     const elenco = document.querySelector ("ul")
 
@@ -25,25 +23,38 @@ document.addEventListener("DOMContentLoaded", () => {
 
 /*
 
-const q = (selector) => document.querySelector(selector);
-const render = (container, items) => {
-    const elements = items.list.map((element, index) => {                     
-    return ` <li>
-    <p><strong>Title:</strong> ${element.title} </p>
-    <p><strong>Completed:</strong> <input type="checkbox" name="check" >${element.completed} </p>
-    </li>` ;
-    }) ;
-    const content = elements.join("");
-    container.innerHTML = content;
 
+
+
+const q = (selector) => document.querySelector(selector);
+
+
+const render = (container, items) => {
+  const elements = items.map((element) => {
+    if(element.completed === true) {
+         `<li>
+        <p><strong>Title:</strong> ${element.title} </p>
+        <p><strong>Completed:</strong> <input type="checkbox" name="check" checked>${element.completed} </p>
+        </li> `;}
+    else {
+        `<li>
+        <p><strong>Title:</strong> ${element.title} </p>
+        <p><strong>Completed:</strong> <input type="checkbox" name="check" >${element.completed} </p>
+        </li> `;};
+   });
+  
+  const content = elements.join('');
+
+  container.innerHTML = content;
 }
 
-document.addEventListener("DOMContentLoaded", () => {     
-     const form = q("form");
-    const input = q("form input");
-    const main = q("main");
-     
-     render(main, list);
+document.addEventListener('DOMContentLoaded', () => {
+  
+  
+  const elenco = q('ul');
+  
+  render(elenco, list);
 });
 
 */
+
